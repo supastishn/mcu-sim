22000ec3aa5d688d3520e6f563af72f3eed46fbd
supastishn <omareeto2012@hotmail.com>
2025-07-10 11:36:50 +0000

    refactor: Improve op-amp simulation stability in nonlinear state transitions

--- components/OpAmp3D.gd
+++ components/OpAmp3D.gd
@@ -75,50 +75,65 @@ func update_nonlinear_state(circuit: CircuitGraph, comp_data: Dictionary, x_iter
 
 	var term_vp = comp_data.terminals["Vp"]
 	var term_vn = comp_data.terminals["Vn"]
+	var term_vout = comp_data.terminals["Vout"]
 	var term_vcc = comp_data.terminals["Vcc"]
 	var term_vee = comp_data.terminals["Vee"]
 
 	var node_vp_id = circuit.terminal_connections.get(term_vp.get_instance_id(), -1)
 	var node_vn_id = circuit.terminal_connections.get(term_vn.get_instance_id(), -1)
+	var node_vout_id = circuit.terminal_connections.get(term_vout.get_instance_id(), -1)
 	var node_vcc_id = circuit.terminal_connections.get(term_vcc.get_instance_id(), -1)
 	var node_vee_id = circuit.terminal_connections.get(term_vee.get_instance_id(), -1)
 
 	var idx_vp = node_map_iter.get(node_vp_id, -1)
 	var idx_vn = node_map_iter.get(node_vn_id, -1)
+	var idx_vout = node_map_iter.get(node_vout_id, -1)
 	var idx_vcc = node_map_iter.get(node_vcc_id, -1)
 	var idx_vee = node_map_iter.get(node_vee_id, -1)
 
 	var Vp = x_iter[idx_vp] if idx_vp != -1 else (0.0 if node_vp_id == circuit.ground_node_id else NAN)
 	var Vn = x_iter[idx_vn] if idx_vn != -1 else (0.0 if node_vn_id == circuit.ground_node_id else NAN)
+	var Vout = x_iter[idx_vout] if idx_vout != -1 else (0.0 if node_vout_id == circuit.ground_node_id else NAN)
 	var Vcc = x_iter[idx_vcc] if idx_vcc != -1 else (0.0 if node_vcc_id == circuit.ground_node_id else NAN)
 	var Vee = x_iter[idx_vee] if idx_vee != -1 else (0.0 if node_vee_id == circuit.ground_node_id else NAN)
 
 	var previous_region = comp_data.properties["operating_region"]
 	var new_region = previous_region
 
-	if is_nan(Vp) or is_nan(Vn) or is_nan(Vcc) or is_nan(Vee):
+	if is_nan(Vp) or is_nan(Vn) or is_nan(Vout) or is_nan(Vcc) or is_nan(Vee):
 		new_region = "OFF"
 	else:
-		var gain = comp_data.properties["open_loop_gain"]
 		var rail_sat_v = comp_data.properties["rail_saturation_voltage"]
+		var sat_high_threshold = Vcc - rail_sat_v
+		var sat_low_threshold = Vee + rail_sat_v
 
-		var V_diff = Vp - Vn
-		var Vout_ideal = V_diff * gain
-
-		var V_sat_high = Vcc - rail_sat_v
-		var V_sat_low = Vee + rail_sat_v
-
-		if Vout_ideal > V_sat_high:
-			new_region = "SAT_HIGH"
-		elif Vout_ideal < V_sat_low:
-			new_region = "SAT_LOW"
-		else:
-			new_region = "LINEAR"
+		if previous_region == "LINEAR":
+			if Vout >= sat_high_threshold - 1e-6:
+				new_region = "SAT_HIGH"
+			elif Vout <= sat_low_threshold + 1e-6:
+				new_region = "SAT_LOW"
+		elif previous_region == "SAT_HIGH":
+			if Vp <= Vn: # Condition to leave high saturation
+				new_region = "LINEAR"
+		elif previous_region == "SAT_LOW":
+			if Vp >= Vn: # Condition to leave low saturation
+				new_region = "LINEAR"
+		else: # Was OFF, initial check
+			var v_diff_for_check = Vp - Vn
+			var gain_for_check = open_loop_gain
+			if gain_for_check > 1e-9:
+				if v_diff_for_check > (sat_high_threshold / gain_for_check):
+					new_region = "SAT_HIGH"
+				elif v_diff_for_check < (sat_low_threshold / gain_for_check):
+					new_region = "SAT_LOW"
+				else:
+					new_region = "LINEAR"
+			else:
+				new_region = "LINEAR"
 
 	if new_region != previous_region:
 		comp_data.properties["operating_region"] = new_region
 		return true
-	
 	return false
 
 func stamp(A: Array, b: Array, node_map: Dictionary, vs_map: Dictionary, _inductor_map: Dictionary, terminal_connections: Dictionary, comp_data: Dictionary, _delta_time: float):
@@ -132,8 +147,6 @@ func stamp(A: Array, b: Array, node_map: Dictionary, vs_map: Dictionary, _induct
 	var idx_vp = node_map.get(terminal_connections.get(terminal_vp.get_instance_id(), -1), -1)
 	var idx_vn = node_map.get(terminal_connections.get(terminal_vn.get_instance_id(), -1), -1)
 	var idx_vout = node_map.get(terminal_connections.get(terminal_vout.get_instance_id(), -1), -1)
-	var idx_vcc = node_map.get(terminal_connections.get(terminal_vcc.get_instance_id(), -1), -1)
-	var idx_vee = node_map.get(terminal_connections.get(terminal_vee.get_instance_id(), -1), -1)
 
 	# Input terminals have very high impedance (ideal op-amp)
 	# Add a very small conductance to ground to prevent floating nodes if inputs are unconnected.
@@ -146,26 +159,37 @@ func stamp(A: Array, b: Array, node_map: Dictionary, vs_map: Dictionary, _induct
 		A[idx_vout][idx_i_out] = 1.0 # Current leaving Vout node
 
 	var region = comp_data.properties["operating_region"]
+	var circuit = comp_data.get("circuit_ref")
 
-	if region == "LINEAR" and idx_vout != -1:
+	if not is_instance_valid(circuit):
+		# Fallback for OFF if circuit ref not available
+		A[idx_i_out][idx_i_out] = 1.0
+		b[idx_i_out] = 0.0
+		return
+
+	if region == "LINEAR":
 		var gain = comp_data.properties["open_loop_gain"]
 		# KVL: Vout - gain * (Vp - Vn) = 0
 		A[idx_i_out][idx_vout] = 1.0
 		if idx_vp != -1:   A[idx_i_out][idx_vp] = -gain
 		if idx_vn != -1:   A[idx_i_out][idx_vn] = gain
 		b[idx_i_out] = 0.0
-	elif region == "SAT_HIGH" and idx_vout != -1:
+	elif region == "SAT_HIGH":
 		var rail_sat_v = comp_data.properties["rail_saturation_voltage"]
-		# KVL: Vout - Vcc = -rail_sat_v
+		var node_vcc_lookup = terminal_connections.get(terminal_vcc.get_instance_id(), -1)
+		var Vcc_prev = circuit.electrical_nodes.get(node_vcc_lookup, {}).get("voltage", 15.0)
+		if is_nan(Vcc_prev): Vcc_prev = 15.0
+		# KVL: Vout = Vcc_prev - rail_sat_v
 		A[idx_i_out][idx_vout] = 1.0
-		if idx_vcc != -1:  A[idx_i_out][idx_vcc] = -1.0
-		b[idx_i_out] = -rail_sat_v
-	elif region == "SAT_LOW" and idx_vout != -1:
+		b[idx_i_out] = Vcc_prev - rail_sat_v
+	elif region == "SAT_LOW":
 		var rail_sat_v = comp_data.properties["rail_saturation_voltage"]
-		# KVL: Vout - Vee = rail_sat_v
+		var node_vee_lookup = terminal_connections.get(terminal_vee.get_instance_id(), -1)
+		var Vee_prev = circuit.electrical_nodes.get(node_vee_lookup, {}).get("voltage", -15.0)
+		if is_nan(Vee_prev): Vee_prev = -15.0
+		# KVL: Vout = Vee_prev + rail_sat_v
 		A[idx_i_out][idx_vout] = 1.0
-		if idx_vee != -1:  A[idx_i_out][idx_vee] = -1.0
-		b[idx_i_out] = rail_sat_v
+		b[idx_i_out] = Vee_prev + rail_sat_v
 	else: # OFF or if Vout is not connected
 		# KVL: i_out = 0 (high impedance output)
 		A[idx_i_out][idx_i_out] = 1.0
