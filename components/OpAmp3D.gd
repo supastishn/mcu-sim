ee3e47331f5661c4a80bb2ce37ec33c9d549aa1a
supastishn <omareeto2012@hotmail.com>
2025-07-13 21:47:17 +0000

    fix: refactor OpAmp3D simulation logic for stable MNA and correct state update

--- components/OpAmp3D.gd
+++ components/OpAmp3D.gd
@@ -23,14 +23,10 @@ signal configuration_changed(component_node: Node3D)
 @onready var terminal_vee: Area3D = $TerminalVee
 ## Reference to the Label3D for displaying simulation info.
 @onready var info_label: Label3D = $InfoLabel
-var _prev_region: String = "OFF"  # Track previous region for change detection
 
 ## Called when the node enters the scene tree. Initializes the component.
 func _ready():
 	hide_info()
-	print_debug("OpAmp3D: Initialized {name} with gain={gain}, saturation={sat}V".format({
-		"name": name, "gain": open_loop_gain, "sat": rail_saturation_voltage
-	}))
 
 # --- Visual Feedback ---
 ## Displays the calculated operating region and voltages on the component's 3D label.
@@ -74,63 +70,55 @@ func update_nonlinear_state(
 		node_map: Dictionary,
 		vs_map: Dictionary
 	) -> bool:
-	
-	print_debug("OpAmp3D({name}): ======= UPDATE NONLINEAR STATE STARTS =======".format({"name": self.name}))
-	
-	print_debug("OpAmp3D({name}): Previous region: {prev}".format({"name": self.name, "prev": _prev_region}))
-	print_debug("OpAmp3D({name}): Solution vec size: {sz}".format({"name": self.name, "sz": solution_vector.size()}))
-	
+
+	if solution_vector.is_empty():
+		return false
+
 	var vp_node_id = circuit.terminal_connections.get(terminal_vp.get_instance_id(), -1)
 	var vn_node_id = circuit.terminal_connections.get(terminal_vn.get_instance_id(), -1)
 	var vcc_node_id = circuit.terminal_connections.get(terminal_vcc.get_instance_id(), -1)
 	var vee_node_id = circuit.terminal_connections.get(terminal_vee.get_instance_id(), -1)
-	
-	var Vp = circuit.electrical_nodes.get(vp_node_id, {}).get("voltage", 0.0)
-	var Vn = circuit.electrical_nodes.get(vn_node_id, {}).get("voltage", 0.0)
-	var Vcc = circuit.electrical_nodes.get(vcc_node_id, {}).get("voltage", 15.0)
-	var Vee = circuit.electrical_nodes.get(vee_node_id, {}).get("voltage", -15.0)
-	
-	print_debug("OpAmp3D({name}): Vp={vp}, Vn={vn}, Vcc={vcc}, Vee={vee}".format({
-		"name": self.name, "vp": Vp, "vn": Vn, "vcc": Vcc, "vee": Vee
-	}))
-	
-	if is_nan(Vcc): Vcc = 15.0
-	if is_nan(Vee): Vee = -15.0
-	if Vcc < Vee: # Swap if rails are inverted
-		var temp = Vcc
-		Vcc = Vee
-		Vee = temp
 
-	var gain = comp_data.properties["open_loop_gain"]
-	var ideal_vout = gain * (Vp - Vn)
+	var vp_idx = node_map.get(vp_node_id, -1)
+	var vn_idx = node_map.get(vn_node_id, -1)
+	var vcc_idx = node_map.get(vcc_node_id, -1)
+	var vee_idx = node_map.get(vee_node_id, -1)
 
-	var rail_drop = comp_data.properties["rail_saturation_voltage"]
-	var high_rail = Vcc - rail_drop
-	var low_rail  = Vee + rail_drop
+	var Vp = solution_vector[vp_idx] if vp_idx != -1 else (0.0 if vp_node_id == circuit.ground_node_id else NAN)
+	var Vn = solution_vector[vn_idx] if vn_idx != -1 else (0.0 if vn_node_id == circuit.ground_node_id else NAN)
+	var Vcc = solution_vector[vcc_idx] if vcc_idx != -1 else (0.0 if vcc_node_id == circuit.ground_node_id else NAN)
+	var Vee = solution_vector[vee_idx] if vee_idx != -1 else (0.0 if vee_node_id == circuit.ground_node_id else NAN)
 
 	var new_region = ""
-	if ideal_vout > high_rail:
-		new_region = "SAT_HIGH"
-	elif ideal_vout < low_rail:
-		new_region = "SAT_LOW"
+	if is_nan(Vp) or is_nan(Vn) or is_nan(Vcc) or is_nan(Vee):
+		new_region = "OFF"
 	else:
-		new_region = "LINEAR"
+		if Vcc < Vee: # Swap if rails are inverted
+			var temp = Vcc
+			Vcc = Vee
+			Vee = temp
 
-	print_debug("OpAmp3D({name}): region={r} (previous={prev}), ideal_vout={iv}, high_rail={hr}, low_rail={lr}".format({
-		"name": self.name, "r": new_region, "prev": comp_data.properties.operating_region,
-		"iv": ideal_vout, "hr": high_rail, "lr": low_rail
-	}))
+		var gain = comp_data.properties["open_loop_gain"]
+		var ideal_vout = gain * (Vp - Vn)
+
+		var rail_drop = comp_data.properties["rail_saturation_voltage"]
+		var high_rail = Vcc - rail_drop
+		var low_rail  = Vee + rail_drop
+
+		if ideal_vout > high_rail:
+			new_region = "SAT_HIGH"
+		elif ideal_vout < low_rail:
+			new_region = "SAT_LOW"
+		else:
+			new_region = "LINEAR"
 
 	var previous_region = comp_data.properties["operating_region"]
 	if new_region != previous_region:
-		print_debug("OpAmp3D({name}): Region changed: {pr} -> {nr}".format({"pr": previous_region, "nr": new_region, "name": self.name}))
 		comp_data.properties["operating_region"] = new_region
-		_prev_region = new_region
-		print_debug("OpAmp3D({name}): Returning true (state changed)".format({"name": self.name}))
 		return true
 
-	print_debug("OpAmp3D({name}): No region change".format({"name": self.name}))
 	return false
+
 
 ## Applies the op-amp's ideal voltage-controlled voltage source model to the MNA matrices.
 func stamp(
@@ -143,7 +131,6 @@ func stamp(
 		comp_data: Dictionary,
 		delta_time: float
 	):
-	# Improved op-amp stamping for numerical stability and convergence
 	var region = comp_data.properties["operating_region"]
 
 	var vp_node_id = terminal_connections.get(terminal_vp.get_instance_id(), -1)
@@ -158,37 +145,44 @@ func stamp(
 	var vcc_idx = node_map.get(vcc_node_id, -1)
 	var vee_idx = node_map.get(vee_node_id, -1)
 
-	var Gbig = 1e12 # Very high conductance
+	var Gbig = 1e9  # Large conductance for voltage stamping
 
-	if region == "LINEAR" or region == "OFF":
-		# Model as voltage follower with gain using feedback resistance
-		var Rf = 1e8  # Large feedback resistance for numerical stability
-		var Rin = 1e6  # Input resistance
-		var open_loop_gain = comp_data.properties["open_loop_gain"]
-
-		if vp_idx != -1 and vn_idx != -1 and vout_idx != -1:
-			# Gain equation approximated with resistors
-			A[vout_idx][vout_idx] += 1.0 / Rf + 1.0 / Rin
-			A[vout_idx][vn_idx] += open_loop_gain / (Rin * Rf)
-			A[vout_idx][vp_idx] += -1.0 / Rin
+	if region == "OFF":
+		if vout_idx != -1:
+			A[vout_idx][vout_idx] += 1e-9 # High impedance to ground
+	elif region == "LINEAR":
+		# Model as a VCVS: Vout = Aol * (Vp - Vn)
+		# Enforced with a large-conductance model: G * (Vout - Aol * (Vp - Vn)) = 0
+		# KCL at Vout: ... + G*Vout - G*Aol*Vp + G*Aol*Vn = 0
+		var Aol = open_loop_gain
+		if vout_idx != -1:
+			A[vout_idx][vout_idx] += Gbig
+			if vp_idx != -1:
+				A[vout_idx][vp_idx] -= Gbig * Aol
+			if vn_idx != -1:
+				A[vout_idx][vn_idx] += Gbig * Aol
 	elif region == "SAT_HIGH":
+		# Enforce Vout = Vcc - rail_saturation_voltage
 		var sat_drop = comp_data.properties["rail_saturation_voltage"]
-		if vout_idx != -1 and vcc_idx != -1:
-			A[vout_idx][vout_idx] += Gbig
-			A[vout_idx][vcc_idx] -= Gbig
-			b[vout_idx] += Gbig * ( -sat_drop )
-		elif vout_idx != -1:  # Vcc not connected, clamp to GND reference
-			A[vout_idx][vout_idx] += Gbig
-			b[vout_idx] += Gbig * ( -sat_drop )
+		if vout_idx != -1:
+			if vcc_idx != -1:
+				A[vout_idx][vout_idx] += Gbig
+				A[vout_idx][vcc_idx] -= Gbig
+				b[vout_idx] += Gbig * ( -sat_drop )
+			else: # Vcc not connected, clamp to GND reference
+				A[vout_idx][vout_idx] += Gbig
+				b[vout_idx] += Gbig * ( -sat_drop )
 	elif region == "SAT_LOW":
+		# Enforce Vout = Vee + rail_saturation_voltage
 		var sat_drop = comp_data.properties["rail_saturation_voltage"]
-		if vout_idx != -1 and vee_idx != -1:
-			A[vout_idx][vout_idx] += Gbig
-			A[vout_idx][vee_idx] -= Gbig
-			b[vout_idx] += Gbig * sat_drop
-		elif vout_idx != -1:  # Vee not connected
-			A[vout_idx][vout_idx] += Gbig
-			b[vout_idx] += Gbig * sat_drop
+		if vout_idx != -1:
+			if vee_idx != -1:
+				A[vout_idx][vout_idx] += Gbig
+				A[vout_idx][vee_idx] -= Gbig
+				b[vout_idx] += Gbig * sat_drop
+			else: # Vee not connected
+				A[vout_idx][vout_idx] += Gbig
+				b[vout_idx] += Gbig * sat_drop
 
 ## Extracts and stores simulation results for this component.
 func gather_sim_results(
@@ -199,10 +193,7 @@ func gather_sim_results(
 		vs_map: Dictionary,
 		inductor_map: Dictionary,
 		delta_time: float):
-	print_debug("OpAmp3D({name}): ======= GATHER RESULTS =======".format({"name": self.name}))
-		
 	var comp_id = self.get_instance_id()
-		
 	var results = {}
 	
 	var vp_node_id = circuit.terminal_connections.get(terminal_vp.get_instance_id(), -1)
@@ -220,16 +211,5 @@ func gather_sim_results(
 		results["Vp_minus_Vn"] = Vp - Vn
 	else:
 		results["Vp_minus_Vn"] = NAN
-	
-	print_debug("OpAmp3D({name}): Vp={vp}, Vn={vn}, Vout={vo}, Vdiff={vd}".format({
-		"name": self.name, "vp": Vp, "vn": Vn, "vo": Vout, "vd": results["Vp_minus_Vn"]
-	}))
-	
-	var region = comp_data.properties.get("operating_region", "N/A")
-	print_debug("OpAmp3D({name}): Operating region: {r}".format({"name": self.name, "r": region}))
 		
 	circuit.component_results[comp_id] = results
-	
-	print_debug("OpAmp3D({name}): Stored results: {res}".format({
-		"name": self.name, "res": circuit.component_results
-	}))
